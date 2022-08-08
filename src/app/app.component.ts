d71b6560d89fe2dee7b9b5df94cc844f77dfa3cc
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-08-08 12:58:22 +0100

    define properties for card component and pass data from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this awesome tree during my hiking today.',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountain',
+      content: 'A picture with these beautiful mountains.',
+    },
+    {
+      title: 'Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking',
+      content: 'I did biking today.',
+    },
+  ];
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  // instructs this class compon that will receive 4 properties with those names
+  // when we want to create it from other component-template file(app.component.html)
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() username = '';
+  @Input() content = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
